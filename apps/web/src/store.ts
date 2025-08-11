9b8b6aa8764a4b2afd16ab5dd1a349a898cf41c3
Palbotics <p.lydick@gmail.com>
2025-08-11 15:18:34 -0400

    fix: updated example JSON path and added viseGripPipeWrench.json

--- apps/web/src/store.ts
+++ apps/web/src/store.ts
@@ -1,6 +1,6 @@
 import create from 'zustand';
 import { Mechanism, createMechanism, solve } from '@linkage-designer/kinematics';
-import data from '../../examples/viseGripPipeWrench.json';
+import data from './examples/viseGripPipeWrench.json';
 
 const initial: Mechanism = solve(createMechanism(data));
 
